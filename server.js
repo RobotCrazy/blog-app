e4052c0ecc7ec5bc219afaa661b4224d07906953
Upneet <captainus55@gmail.com>
2019-07-31 22:19:43 -0400

    Beginning to add login functionality

--- server.js
+++ server.js
@@ -2,10 +2,46 @@ const express = require('express');
 const app = express();
 const dbConnection = require('./blogScripts/databaseConnection');
 const bodyParser = require('body-parser');
+const session = require('express-session');
+const passport = require('passport');
+const expressValidator = require('express-validator');
+const LocalStrategy = require('passport-local').Strategy;
+const multer = require('multer');
+var upload = multer({ dest: './uploads' });
+const flash = require('connect-flash');
 
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
+app.use(session({
+    secret: "secret",
+    saveUninitialized: true,
+    resave: true
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+// app.use(expressValidator({
+//     errorFormatter: function(param, msg, value) {
+//         var namespace = param.split('.'),
+//             root = namespace.shift(),
+//             formParam = root;
+
+//         while (namespace.length) {
+//             formParam += '[' + namespace.shift() + ']';
+//         }
+
+//         return {
+//             param: formParam,
+//             msg: msg,
+//             value: value
+//         };
+//     }
+// }));
+app.use(require('connect-flash')());
+app.use(function(req, res, next) {
+    res.locals.messages = require('express-messages')(req, res);
+    next();
+});
 
 app.get('/', function(req, res) {
     let dbData;
